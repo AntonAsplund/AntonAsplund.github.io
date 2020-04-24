e99e4600e30e52ed685e96bafa07ddbe2fc800c1
Anton Asplund <tomte_123@hotmail.com>
2020-04-24 14:44:08 +0200

    Product picture fixed with js

---
+++ LaborationOne/Product page/script.js
@@ -0,0 +1,8 @@
+function productOneFunction() {
+    var x = document.getElementById("productonecloseup");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
